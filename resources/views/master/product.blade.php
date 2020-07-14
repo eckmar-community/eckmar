9158419ac7b97e798e5cb35f1ba6198c47014e46
Dan Clipca <virtumonde@mac.local>
2020-07-14 16:24:37 -0400

    Update product blade

--- resources/views/master/product.blade.php
+++ resources/views/master/product.blade.php
@@ -184,21 +184,23 @@
                             @endif
                             </td>
                             <td class="row">
+                                @if($product -> isUnlimited())
                                 <div class="col-md-5">
-                                    @if($product -> isUnlimited())
                                     <input style="display: none;" type="number" min="1" name="amount" id="amount"
                                            value="1"
                                            max="{{ $product -> quantity }}"
                                            class="@if($errors -> has('amount')) is-invalid @endif form-control form-control-sm"
                                            placeholder="Amount of {{ str_plural($product -> mesure) }}"/>
-                                    @else
+                                </div>
+                                @else
+                                <div class="col-md-5">
                                     <input type="number" min="1" name="amount" id="amount"
                                            value="1"
                                            max="{{ $product -> quantity }}"
                                            class="@if($errors -> has('amount')) is-invalid @endif form-control form-control-sm"
                                            placeholder="Amount of {{ str_plural($product -> mesure) }}"/>
-                                    @endif
-                                </div>
+                                />
+                                @endif
                                 <div class="col-md-7">
                                     <button class="btn btn-sm btn-block mb-2 btn-primary"><i class="fas fa-plus mr-2"></i>Add to
                                         cart
