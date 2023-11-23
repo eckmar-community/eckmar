f4418bd59854e339bff00981ecf5d9e0ae56893b
knulli <anno@kali.kali.yerbas.org>
2023-11-23 10:12:47 -0800

    Branch Fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,11 +28,15 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // Schedule to delete old messages every old days
-    //    $schedule -> command(DeleteOldMessages::class, ['days' => config('marketplace.days_old_messages')])->days(config('marketplace.days_old_messages'));
+    //    $schedule -> command(DeleteOldMessages::class, ['days' => config('marketplace.days_old_messages')])
+    //                ->days(config('marketplace.days_old_messages'));
 
-        $schedule -> command(CompletePurchaseCommand::class) -> days(config('marketplace.days_complete'));
+        // Make the command for releasing purchases runs each X days
+        $schedule -> command(ReleasePurchasesCommand::class, ['days' => config('marketplace.days_old_purchases')])
+                    ->days(config('marketplace.days_old_purchases'));
+
+        // Run completing command for purchases every defined number of days
         $schedule -> command(ReleasePurchasesCommand::class, ['days' => config('marketplace.days_old_purchases')])->daily();
-        
 
     }
 
